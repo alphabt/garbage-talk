b369bf33e2fe4616ac789662703baf343c66eaf8
Brian Tai <alphabt@gmail.com>
2015-12-12 01:11:24 +0800

    Add bootstrap styling

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.4'
 gem 'pg', group: :production
 gem 'rails_12factor', group: :production
+gem 'bootstrap-sass', '~> 3.3.6'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+  BOOTSTRAP_FLASH_MSG = {
+    success: 'alert-success',
+    notice: 'alert-info',
+    alert: 'alert-warning',
+    error: 'alert-danger',
+  }
+
+  def bootstrap_class_for(flash_type)
+    BOOTSTRAP_FLASH_MSG.fetch(flash_type.to_sym, flash_type.to_s)
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: "alert") do
+        concat(content_tag(:button, '', type: "button", class: "close", data: { dismiss: 'alert' }, aria: {label: 'Close'}) do
+          concat content_tag(:span, '×', aria: {hidden: 'Close'})
+        end)
+        concat message
+      end)
+    end
+    nil
+  end
 end
